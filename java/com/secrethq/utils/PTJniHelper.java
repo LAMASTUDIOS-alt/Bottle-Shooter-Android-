a8a0aafcf4d1b215201f8cd840ee3a102ae21ea1
LAMASTUDIOS-alt <83341151+LAMASTUDIOS-alt@users.noreply.github.com>
2022-03-20 17:42:26 -0500

    Add files via upload

---
+++ java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "HneqcJ+utgsaL6kuyaqyUB97rXSZ/r0KTHuqc5qotlEZKK5zzqy8XRgsrC/JrLRYSH78JZn7sVkcePBzyv3mDA==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
